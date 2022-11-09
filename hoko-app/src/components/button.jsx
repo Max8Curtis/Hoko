a999f6544242e63eba32216b313402fca170b14d
Max8Curtis <mcurtis01@outlook.com>
2022-11-09 17:47:29 +0000

    Added tooltips for buttons

--- hoko-app/src/components/button.jsx
+++ hoko-app/src/components/button.jsx
@@ -1,12 +1,19 @@
 import React, { Component, useState } from 'react';
+import './styles.css'
+import Tooltip from '@mui/material/Tooltip';
 
-function Button({bid, buttonImage}) {
+function Button({bid, buttonImage, buttonWidth, buttonHeight, buttonText, imageWidth, tooltipText}) {
 
     return (
         <div>
-            <button style={{width: 70, height: 70, borderRadius: 8}}>
-                <img src={buttonImage} style={{width: 50}}></img>
-            </button>
+            <Tooltip title={tooltipText} placement="right" arrow>
+                <button style={{width: buttonWidth, height: buttonHeight, borderRadius: 8}}>
+                    <div className="flex-container">
+                        <p style={{marginTop: -5}}>{buttonText}</p>
+                        <img src={buttonImage} style={{paddingLeft: 5, width: imageWidth, height: '100%'}}></img>
+                    </div>
+                </button>
+                </Tooltip>
         </div>
     );
 }
--- hoko-app/src/pages/main/main.jsx
+++ hoko-app/src/pages/main/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
-import { TitleWrapper, Title } from './styles'
+import { TitleWrapper, Title, Label } from './styles'
 import Button from '../../components/button'
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -9,6 +9,8 @@ import resetImage from '../../assets/restart_icon.png'
 import micImage from '../../assets/mic_icon.png'
 import helpImage from '../../assets/help_icon.png'
 import undoImage from '../../assets/undo_icon.png'
+import '../../App.css'
+import Dialog from '../../components/dialog'
 
 // const Item = styled(Paper)(({ theme }) => ({
 //     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -24,31 +26,45 @@ function Main() {
             <Grid container direction="column" justifyContent="flex-start" alignItems="center">
                 <Grid item xs={12}>
                     {/* <item>1</item> */}
-                    <h1 style={{fontSize: 60}}>方向</h1>
+                    <h1 style={{fontSize: 60, fontFamily:'MS Mincho'}}>方向</h1>
                 </Grid>
                 <Grid item xs={12}>
                     {/* <item>2</item> */}
-                    <h2>Hōkō</h2>
+                    <h2 style={{fontSize: 40, fontFamily:'MS Mincho'}}>Hōkō</h2>
                 </Grid>
-                <Grid item xs={12} style={{marginTop:50}}>
-                    <Grid container justifyContent="center" alignItems="center" spacing={4}>
+                <Grid item xs={12} style={{marginTop:30}}>
+                    <Grid container justifyContent="center" alignItems="center" spacing={12}>
                         <Grid item x={1}>
                             <Grid container justifyContent="center" direction="column" spacing={2} alignItems="baseline">
                                 <Grid item>
-                                    <Button item xs={1} bid={0} buttonImage={resetImage}/>
+                                    <Button item xs={1} bid={0} buttonImage={resetImage} buttonWidth={70} buttonHeight={70} imageWidth={50} tooltipText={'Reset to start'}/>
+                                    <h2 className='button-label'>Reset</h2>
                                 </Grid>
                                 <Grid item>
-                                    <Button item xs={1} bid={1} buttonImage={undoImage}/>
+                                    <Button item xs={1} bid={1} buttonImage={undoImage} buttonWidth={70} buttonHeight={70} imageWidth={50} tooltipText={'Undo previous move'}/>
+                                    <h2 className='button-label' style={{paddingLeft:8}}>Undo</h2>
                                 </Grid>
                                 <Grid item>
-                                    <Button item xs={1} bid={2} buttonImage={micImage}/>
+                                    <Button item xs={1} bid={2} buttonImage={micImage} buttonWidth={70} buttonHeight={70} imageWidth={50} tooltipText={'Speak to move character'}/>
+                                    <h2 className='button-label'>Speak</h2>
                                 </Grid>
                             </Grid>
                         </Grid>
                         <Grid item>
                             <div style={{height:400, width:700,backgroundColor:'blue', borderRadius:8}}></div>
                         </Grid>
+                        <Grid item>
+                            <Button item xs={1} bid={3} buttonImage={helpImage} buttonWidth={70} buttonHeight={70} imageWidth={50} tooltipText={'How to play'}/>
+                            <h2 className='button-label' style={{paddingLeft:8}}>Help</h2>
+                        </Grid>
                     </Grid>
+                </Grid>
+                <Grid item xs={12} style={{marginTop:20}}>
+                    <Grid container direction="column" alignItems="flex-end">
+                        <Dialog/>
+                        <Button item xs={1} bid={3} buttonImage={resetImage} buttonWidth={90} buttonHeight={25} buttonText={'Romaji'} imageWidth={20} tooltipText={'Switch between Japanese scripts'}/>
+                    </Grid>
+                    
                 </Grid>
             </Grid>
         </Box>
